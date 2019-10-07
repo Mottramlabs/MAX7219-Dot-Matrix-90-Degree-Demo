3411af1748d897f17eec0f45f8e66bc83e346e39
David Mottram <Mottramlabs@users.noreply.github.com>
2019-10-07 11:13:59 +0100

    Update MAX7219_90_Deg_Demo.ino

--- MAX7219_90_Deg_Demo/MAX7219_90_Deg_Demo.ino
+++ MAX7219_90_Deg_Demo/MAX7219_90_Deg_Demo.ino
@@ -12,7 +12,7 @@
           +5V       5V            VCC
           GND       GND           GND
   DIN     D11/MOSI  GPIO13 (D7)   DIN
-  CS      *D7/SS    *GPIO15 (D8)   CS
+  CS      *D10/SS    *GPIO15 (D8)   CS
   CLK     D13/SCK   GPIO14 (D5)   CLK
 
 */
